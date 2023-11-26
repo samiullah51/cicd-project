7e97c8390552697e4e4d4050b8c43b26acb15a97
samiullah51 <samii51728@gmail.com>
2023-11-26 16:18:08 +0000

    test done

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req,res)=>{
 res.status(201).json({success:true, data:{message:"Congratulation bro!!!"}})
 })
 
+app.get('/send', (req,res)=>{
+res.status(201).json({success:true, data:{message:"Message has been sent successfully"}})
+
+})
 
 app.listen(8000, ()=>{
     console.log('server is running on port 8000')
